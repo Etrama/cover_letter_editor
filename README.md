da8508c4e660b60d51d1e04db949fcbccc56ef72
Etrama <kaushikmoudgalya@gmail.com>
2023-03-13 19:30:58 -0400

    cover letter editor - good to go

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple Cover Letter generator. 
+<br>
+<br>
+It uses a template file to generate a cover letter based on the information you provide.
+<br>
+<br>
+The idea is to ease the just replace the company name and position name for each position that you apply to, since it is practically impossible to write a cover letter for each job that you want to apply to.
+<br>
+<br>
+The requirements.txt file is a bit overkill in this case, but it is there if you need it. Simply put, you only need docx, python-docx, re and pysimplegui to be installed on your system.
+<br>
+<br>
+Once you have this script (I use Windows), create a shortcut of the "cover_letter_script.py" file by right-clicking on it. Right click on the shortcut and select "Properties". In the "Shortcut Key" field, hold whatever combination of keys you want to use to run the script. I use "Ctrl + Alt + C" for example.
+<br>
+<br>
+Feel free to use, redistribute, modify, etc. this script. I hope it helps you in your job search. I hope it helps my job search too :D.
---
+++ cover_letter_script.py
@@ -0,0 +1,33 @@
+import re
+from docx import Document
+from docx.shared import Pt
+import PySimpleGUI as gui
+
+FILE_SRC = r"C:\Users\kaush\Desktop\Resumes\Cover Letters\Script\0_Cover letter_Kaushik Moudgalya_v2.docx"
+#get company name from the user:
+COMPANY = gui.PopupGetText("Enter the company name: ")
+POSITION = gui.PopupGetText("Enter the position name: ")
+# TO DO: we need a dialog box that will ask me to enter the company name
+FILE_DEST = r"C:\Users\kaush\Desktop\Resumes\Cover Letters\Script\\" + str(COMPANY) + r"_Cover letter_Kaushik Moudgalya_v2.docx"
+
+# replace the "Position" and the "Company" strings.
+document = Document(FILE_SRC)
+# some stuff for the font
+style = document.styles['Normal']
+font = style.font
+font.name = 'Arial'
+font.size = Pt(10.5)
+# loop through the paragraphs and replace the strings
+for index, para in enumerate(document.paragraphs):
+    # print(index, para.text)
+    if "Position" and "Company" in para.text:
+        # print(para.text)
+        new_text = re.sub("Position", POSITION, para.text)
+        new_text = re.sub("Company", COMPANY, new_text)
+        para.text = new_text
+        para.style = document.styles['Normal']
+        break
+# following code tests if the strings are replaced
+# for index, para in enumerate(document.paragraphs):
+#     print(index, para.text)
+document.save(FILE_DEST)
