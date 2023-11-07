b621878e280e443aeaff80666e9438cb78190d68
Etrama <kaushikmoudgalya@gmail.com>
2023-11-06 19:50:23 -0500

    bold text, template modify

--- README.md
+++ README.md
@@ -7,10 +7,32 @@ It uses a template file to generate a cover letter based on the information you 
 The idea is to ease the just replace the company name and position name for each position that you apply to, since it is practically impossible to write a cover letter for each job that you want to apply to. Sadly, the current job market is a numbers game more than anything else, so here we are.
 <br>
 <br>
-The requirements.txt file is a bit overkill in this case, but it is there if you need it. Simply put, you only need docx, python-docx, re and pysimplegui to be installed on your system.
+The requirements.txt file is there if you need it. Simply put, you only need docx, python-docx, re and pysimplegui to be installed on your system.
 <br>
 <br>
 Once you have this script (I use Windows), create a shortcut of the "cover_letter_script.py" file by right-clicking on it. Right click on the shortcut and select "Properties". In the "Shortcut Key" field, hold whatever combination of keys you want to use to run the script. I use "Ctrl + Alt + C" for example.
 <br>
 <br>
 Feel free to use, redistribute, modify, etc. this script. I hope it helps you in your job search. I hope it helps my job search too :D.
+<br>
+<br>
+I am attaching a sample of the folder structure that I use for this project so that everyone else can also use it:
+<br>
+cover_letter_editor/
+│
+├── generated_cover_letters/
+|   ├── .gitkeep
+│   ├── generated_output_1.docx
+│   ├── generated_output_2.docx
+│   ├── ....
+│   └── generated_output_n_.docs
+│
+├── template_cover_letter/
+|   ├── .gitkeep
+│   └── YOUR_TEMPLATE_COVER_LETTER.docx
+│
+├── .gitignore
+├── cover_letter_script.ipynb
+├── cover_letter_script.py
+├── README.md
+└── requirements.txt
--- cover_letter_script.py
+++ cover_letter_script.py
@@ -3,12 +3,12 @@ from docx import Document
 from docx.shared import Pt
 import PySimpleGUI as gui
 
-FILE_SRC = r"C:\Users\kaush\Desktop\Resumes\Cover Letters\Script\0_Cover letter_Kaushik Moudgalya_v2.docx"
+FILE_SRC = r".\template_cover_letter\0_Cover letter_Kaushik Moudgalya_v2.docx"
 #get company name from the user:
 COMPANY = gui.PopupGetText("Enter the company name: ")
 POSITION = gui.PopupGetText("Enter the position name: ")
 # TO DO: we need a dialog box that will ask me to enter the company name
-FILE_DEST = r"C:\Users\kaush\Desktop\Resumes\Cover Letters\generated_cover_letters\\" + str(COMPANY) + r"_Cover letter_Kaushik Moudgalya_v2.docx"
+FILE_DEST = r"./generated_cover_letters//" + str(COMPANY) + r"_Cover letter_Kaushik Moudgalya_v2.docx"
 
 # replace the "Position" and the "Company" strings.
 document = Document(FILE_SRC)
@@ -16,18 +16,20 @@ document = Document(FILE_SRC)
 style = document.styles['Normal']
 font = style.font
 font.name, font.size = 'Arial', Pt(10.5)
-# font.size = Pt(10.5)
-# loop through the paragraphs and replace the strings
 for index, para in enumerate(document.paragraphs):
-    # print(index, para.text)
-    if "Position" and "Company" in para.text:
-        # print(para.text)
-        new_text = re.sub("Position", POSITION, para.text)
-        new_text = re.sub("Company", COMPANY, new_text)
-        para.text = new_text
+    if "Position" in para.text and "Company" in para.text:
+        runs = para.runs
+        for run in runs:
+            if "Position" in run.text:
+                # Find and replace "Position" with the user input
+                new_text = run.text.replace("Position", POSITION)
+                run.text = new_text
+                run.bold = True  # Set the bold formatting for "POSITION"
+            if "Company" in run.text:
+                # Find and replace "Company" with the user input
+                new_text = run.text.replace("Company", COMPANY)
+                run.text = new_text
+                run.bold = True  # Set the bold formatting for "COMPANY"
         para.style = style
         break
-# following code tests if the strings are replaced
-# for index, para in enumerate(document.paragraphs):
-#     print(index, para.text)
 document.save(FILE_DEST)
